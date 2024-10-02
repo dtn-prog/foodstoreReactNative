e33ce186f86dbe2104fca5e85f54a122a96a1aa8
dtn-prog <dtncode@gmail.com>
2024-10-02 22:28:29 +0700

    add stack navigator for produc tdetail screen

--- App.js
+++ App.js
@@ -3,10 +3,20 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Entypo from '@expo/vector-icons/Entypo';
+import ProductDetailScreen from './screens/ProductDetailScreen';
 
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
+
+const HomeStack = () => {
+  return(
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen}/>
+      <Stack.Screen name="ProductDetail" component={ProductDetailScreen}/>
+    </Stack.Navigator>
+  )
+};
 
 export default function App() {
   return (
@@ -14,7 +24,7 @@ export default function App() {
       <Tab.Navigator screenOptions={{
         headerShown: false, tabBarActiveTintColor: 'blue'
       }}>
-        <Tab.Screen name="Home" component={HomeScreen} 
+        <Tab.Screen name="HomeStack" component={HomeStack} 
         options={{tabBarIcon:()=><Entypo name="home" size={24} color="black" />}}/>
         <Tab.Screen name="Reorder" component={HomeScreen} 
         options={{tabBarIcon:()=><Entypo name="list" size={24} color="black" />}}/>
--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -1,9 +1,14 @@
 import { View, Image, Text } from 'react-native';
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { TouchableOpacity } from 'react-native';
 
 const ProductCard = ({ name, price, image }) => {
+  const navigation = useNavigation();
+
   return (
-    <View className="overflow-hidden flex-1 m-2 bg-white rounded-lg shadow-md"> 
+    <TouchableOpacity onPress={()=>{navigation.navigate('ProductDetail')}}
+    className="overflow-hidden flex-1 m-2 bg-white rounded-lg shadow-md"> 
       <Image
         source={image}
         className="object-cover w-full h-40" 
@@ -13,7 +18,7 @@ const ProductCard = ({ name, price, image }) => {
         <Text className="text-lg font-semibold">{name}</Text>
         <Text className="text-base text-gray-600">{price}</Text>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 };
 
